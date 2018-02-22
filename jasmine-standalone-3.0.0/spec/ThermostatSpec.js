9f0edc47fd69205e149e6ebec463b57664eb7736
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-22 16:05:53 +0000

    tests written and passing for power saving switch and error raising. Test written for energy usage

--- jasmine-standalone-3.0.0/spec/ThermostatSpec.js
+++ jasmine-standalone-3.0.0/spec/ThermostatSpec.js
@@ -6,20 +6,44 @@ describe('Thermostat', function() {
   });
 
   it('has a default temperature of 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.temperature).toEqual(20);
   });
 
   it('temperature is increased by provided value', function(){
-    thermostat.up_temp(10)
-    expect(thermostat.temperature).toEqual(30)
+    thermostat.up_temp(3);
+    expect(thermostat.temperature).toEqual(23);
   });
 
   it('temperature is decreased by provided value', function(){
-    thermostat.down_temp(5)
-    expect(thermostat.temperature).toEqual(15)
+    thermostat.down_temp(5);
+    expect(thermostat.temperature).toEqual(15);
   });
 
   it('raises an error if temperature is going to fall below 10 degrees', function(){
-    expect(function(){thermostat.down_temp(11)}).toThrow('Temperature is too low!')
+    expect(function(){thermostat.down_temp(11);}).toThrow('Temperature is too low!');
+  });
+
+  it('Power saving on: raises an error if temperature goes above 25 degrees', function(){
+    expect(function(){thermostat.up_temp(10);}).toThrow('Temperature is too high!');
+  });
+
+  it('Power saving on: does NOT raise an error if temperature goes above 25 and below 32 degrees', function(){
+    thermostat._powerSavingSwitch();
+    expect(function(){thermostat.up_temp(10);}).not.toThrow('Temperature is too high!');
+  });
+
+  it('Power saving off: raises an error if temperature goes above 32 degrees', function(){
+    expect(function(){thermostat.up_temp(25);}).toThrow('Temperature is too high!');
+  });
+
+  it('Resets temperature to 20 when reset function is called', function(){
+    thermostat.up_temp(5);
+    thermostat.reset_temperature();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('returns low usage if temperate < 18', function() {
+    thermostat.down_temp(5);
+    expect(thermostat.currentUsage).toEqual('Low usage');
   });
 });
--- jasmine-standalone-3.0.0/src/Thermostat.js
+++ jasmine-standalone-3.0.0/src/Thermostat.js
@@ -1,11 +1,25 @@
 var Thermostat = function() {
   this.temperature = 20;
+  this.powerSaving = true;
 };
 
 Thermostat.prototype.up_temp = function(degrees){
-  this.temperature += degrees;
+  if(this.powerSaving) {
+      if((this.temperature += degrees) > 25) throw 'Temperature is too high!';
+  }
+  else {
+    if((this.temperature += degrees) > 32) throw 'Temperature is too high!';
+  }
 };
 
 Thermostat.prototype.down_temp = function (degrees) {
   if((this.temperature -= degrees) < 10) throw 'Temperature is too low!';
+};
+
+Thermostat.prototype._powerSavingSwitch = function() {
+  this.powerSaving = !(this.powerSaving);
+};
+
+Thermostat.prototype.reset_temperature = function () {
+  this.temperature = 20;
 };
