b8af7f86e310131a767fa71af8c0b7be045d1537
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-23 16:30:14 +0000

    buttons attached to javascript, working

--- jasmine-standalone-3.0.0/src/interface.js
+++ jasmine-standalone-3.0.0/src/interface.js
@@ -1,12 +1,30 @@
-
-$(document).ready(function () {   
+$(document).ready(function () {
   var thermostat = new Thermostat();
-  // document.getElementById("12345").innerHTML = thermostat.getCurrentTemperature();
   $('#12345').text("Current temperature is: " + thermostat.temperature);
+    $('.2').text("Power saving switch is on: " + thermostat.powerSaving);
   $( '#temperature-up' ).click(function() {
     thermostat.upTempOne();
     $('#12345').text("Current temperature is: " + thermostat.temperature);
-    
   });
+
+  $( '#temperature-down' ).click(function() {
+    thermostat.downTempOne();
+    $('#12345').text("Current temperature is: " + thermostat.temperature);
+  });
+
+  $( '#temperature-reset' ).click(function() {
+    thermostat.resetTemperature();
+    $('#12345').text("Current temperature is: " + thermostat.temperature);
+  });
+
+  $( '#power-saving-switch' ).click(function() {
+    thermostat._powerSavingSwitch();
+    $('#12345').text("Current temperature is: " + thermostat.temperature);
+    $('.2').text("Power saving switch is on: " + thermostat.powerSaving);
+  });
+
 });
 
+// powerSaveDisplay = function() {
+//   return thermostat.powerSaving ? 'On' : 'Off';
+// };
